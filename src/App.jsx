290a3bf99a8ccdc8c608c399727859c13be4a6c8
Alhaadii <alcazaam1233@gmail.com>
2025-01-01 23:44:12 -0800

    add a footer for todo app

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ const App = () => {
         <div className="col-lg-6 col-md-10 col-12 mx-auto bg-light p-5 shadow">
           <TodoForm />
           <TodoList />
+          <p className="text-center text-primary">Copyright &copy;2025</p>
         </div>
       </div>
     </div>
